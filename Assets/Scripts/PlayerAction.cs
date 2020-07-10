31bb74540e11e0f8b0c29d2428e6e958bde55bb6
halohoang <62345058+halohoang@users.noreply.github.com>
2020-07-10 10:57:16 +0200

    Animation state & Horizontal Movement
    + Implement all movement animation including logic (no death and attack animation yet)
    + Create and code the basic horizontal movement of the player character

---
+++ Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SAE_Project
+{
+	public class PlayerAction : MonoBehaviour
+	{
+		// Variables
+		
+
+		// Functions
+		void Start( )
+		{
+
+		}
+		void Update( )
+		{
+			//Move the sprite in horizontal direction
+			transform.Translate(Input.GetAxis("Horizontal") * 15f * Time.deltaTime, 0f, 0f);
+
+			//Flip the Sprite changing in horizontal direction
+
+			Vector3 characterScale = transform.localScale;
+			if (Input.GetAxis ("Horizontal") < 0)
+			{
+				characterScale.x = -6;
+			}
+			if (Input.GetAxis ("Horizontal") > 0)
+			{
+				characterScale.x = 6;
+			}
+			transform.localScale = characterScale;
+		}
+	}
+}
+
+	
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using SAE_Project;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SAE_Project
+{
+public class PlayerMovement : MovementBase
+  {
+        protected override void CalculatedDirection( )
+        {
+            _direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+
+
+   }
+
+
+}
