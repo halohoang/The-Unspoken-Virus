c59a4e9c38ccca59acd258d5093c44bf1647ea15
Pedro Mayz <pedromb04@gmail.com>
2020-08-29 17:15:05 +0200

    Making a raycast for the projectile
    Made a raycast for the enemy that the player cannot kill the enemy with projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -13,11 +13,18 @@ namespace SAE_Project
 		public Faction Team;
 		[SerializeField]
 		private int _damage;
-
+		private Transform attackPoint;
+		private float _attackRange;
 
 		//Functions
 		private void OnTriggerEnter2D(Collider2D collision)
 		{
+			RaycastHit2D hitShield = Physics2D.Raycast(transform.position, Vector2.right, Mathf.Abs(transform.position.x - attackPoint.position.x) + _attackRange);
+
+			if (hitShield.collider != null && hitShield.collider.CompareTag("Shield"))
+			{
+				return;
+			}
 			if (collision.TryGetComponent(out IDamageable damage))
 			{
 				if (damage.GetFaction() != Team )
