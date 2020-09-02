f7c25fd349be01fa638d03edde562b2ddfee954f
Hayoon <hayoonee@gmail.com>
2020-09-02 21:20:05 +0900

    push for the update. making lv1, added parallax script and fixed boss lv

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallex : MonoBehaviour
+{
+    [SerializeField] private float parallaxEffectMultiplier;
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+
+            }
+
+    private void Latepdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        float parallaxEffectMultipiler = 0.5f;
+        transform.position += deltaMovement;
+        lastCameraPosition = cameraTransform.position;
+    }
+
+}
