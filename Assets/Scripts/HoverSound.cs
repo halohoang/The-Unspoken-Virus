9bd585812bf0ec6e8201a898bb3bc501283132ef
Hoang <halohoang@gmail.com>
2020-09-02 15:22:37 +0200

    Hover Sound Effect
    +Add sound effect when hover over buttons

---
+++ Assets/Scripts/HoverSound.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SAE_Project.Assets.Scripts
+{
+    class HoverSound : MonoBehaviour
+    {
+        //variables
+        public AudioSource Effect;
+        public AudioClip HoverEffect;
+
+
+        //Functions
+        public void HoverEffectSound()
+        {
+            Effect.PlayOneShot(HoverEffect);
+        }
+
+    }
+}
