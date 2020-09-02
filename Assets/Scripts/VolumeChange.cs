65191e3f8b9d6eb074701b51029b9677e08141da
Hoang <halohoang@gmail.com>
2020-09-02 14:58:51 +0200

    Menu BG music + volume slider
    + Add music to the menu scene
    + Add function to change volume sound with the slider

---
+++ Assets/Scripts/VolumeChange.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace SAE_Project.Assets.Scripts
+{
+    class VolumeChange : MonoBehaviour
+    {
+        //Variables
+        private AudioSource _audioSource;
+
+        private float _musicVolume = 1f;
+
+        //functions
+        private void Start()
+        {
+            //Assign AudioSource component to control it
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        private void Update()
+        {
+            //Setting volume option of AudioSource to be equal to _musicVolume to control it;
+            _audioSource.volume = _musicVolume;
+        }
+
+        public void SetVolume(float vol)
+        {
+            _musicVolume = vol; 
+        }
+
+    }
+}
