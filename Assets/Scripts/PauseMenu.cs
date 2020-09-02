f3ef21b738f9921b08e565b5a686c6116a27eba4
Hoang <halohoang@gmail.com>
2020-09-03 01:51:53 +0200

    Add basic pause menu gui and functions
    + Add GUI for Pause Menu:
     - Resume
     - Volume
     - Restart (Requires HY)
     - Quit
     - Menu (Bug)

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SAE_Project
+{
+
+    public class PauseMenu : MonoBehaviour
+    {
+        //Variables
+        public static bool GameIsPaused = false;
+        public GameObject pauseMenuUI;
+
+
+
+        //Functions
+        public void Resume()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            GameIsPaused = false;
+        }
+
+        public void Pause()
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+        }
+        public void LoadMenu()
+        {
+            SceneManager.LoadScene("Menu");
+            Debug.Log("Back to Menu");
+        }
+
+        public void Quit()
+        {
+            Debug.Log("Quitting Game");
+            Application.Quit();
+
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (GameIsPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+    }
+
+}
