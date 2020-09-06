2a05b26ed5a1254e62f2f29fa088428ae1b32fbd
Hayoon <hayoonee@gmail.com>
2020-09-06 13:13:40 +0900

    added background of background

---
+++ Assets/Scripts/InteractiveSignboard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveSignboard : MonoBehaviour
+{
+    
+
+    private bool _playerInteract;
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+
+            _playerInteract = true;
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            _playerInteract = false;
+
+        }
+    }
+}
